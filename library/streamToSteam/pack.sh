6382576061f8a9d6870fe422843df134bb701985
scottcolson1968 <scottwcolson@gmail.com>
2026-07-03 20:31:17 -0400

    Revert "Revert "Revert "Genesys ZU: 16-bit samples to halve wire throughput"""
    This reverts commit 6a4c957833109c8af6e8ff75180fa9cee7628a91.

--- library/streamToSteam/pack.sh
+++ library/streamToSteam/pack.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # ==============================================================
-# Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2025.1 (64-bit)
-# Tool Version Limit: 2025.05
+# Vitis HLS - High-Level Synthesis from C, C++ and OpenCL v2024.1 (64-bit)
+# Tool Version Limit: 2024.05
 # Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
-# Copyright 2022-2025 Advanced Micro Devices, Inc. All Rights Reserved.
+# Copyright 2022-2024 Advanced Micro Devices, Inc. All Rights Reserved.
 # 
 # ==============================================================
 
-/opt/Xilinx/2025.1/Vivado/bin/vivado  -notrace -mode batch -source run_ippack.tcl
+/home/analog/tools/Xilinx/Vivado/2024.1/bin/vivado  -notrace -mode batch -source run_ippack.tcl
